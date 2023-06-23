ebcff5b552482d057922564d3f91588040ec5844
millalisredyaid <milanaxrazdukhova@gmail.com>
2023-06-24 07:30:59 +0900

    JSON形式のレスポンス記述

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,11 @@ class CategoriesController < ApplicationController
     @categories = Category.new
     @maincategories = Category.all.order("id ASC").limit(13)
   end
+
+  def search
+    item = Category.find(params[:id])
+    children_item = item.children
+    render json:{ item: children_item }
+  end
+  
 end
