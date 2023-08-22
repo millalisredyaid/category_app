c910d073751d7b6fe6e1f4dcf5c065eb2b7f7e18
millalisredyaid <milanaxrazdukhova@gmail.com>
2023-08-22 16:48:41 +0900

    end

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,8 @@ class CategoriesController < ApplicationController
   def search
     item = Category.find(params[:id])
     children_item = item.children
+    
     render json:{ item: children_item }
   end
-  
+
 end
--- app/javascript/category.js
+++ app/javascript/category.js
@@ -1,27 +1,29 @@
 window.addEventListener('load', function () {
 
   const parentCategory = document.getElementById('parent-category')
-
+ 
   const selectChildElement = (selectForm) => {
-
+ 
   }
-
-  const XHR = new XMLHttpRequest();
-  const categoryXHR = (id) => {
-    XHR.open("GET", `/category/${id}`, true);
-    XHR.responseType = "json";
-    XHR.send();
-  }
-
-
+ 
+   const XHR = new XMLHttpRequest();
+   const categoryXHR = (id) => {
+     XHR.open("GET", `/category/${id}`, true);
+     XHR.responseType = "json";
+     XHR.send();
+   }
+ 
   const getChildCategoryData = () => {
     const parentValue = parentCategory.value
-    categoryXHR(parentValue)
-  }
-
+     categoryXHR(parentValue)
+ 
+     XHR.onload = () => {
+       const items = XHR.response.item;
+   }
+ }
+ 
   parentCategory.addEventListener('change', function () {
     selectChildElement('child-select-wrap')
     getChildCategoryData()
   })
-
-})
+ })
